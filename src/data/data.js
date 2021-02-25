0429347867ffbaa81cf896607fd774d4fd768df1
Sebastian Kann <sebastiankann@gmail.com>
2021-02-25 13:06:06 -0500

    data coded

--- src/data/data.js
+++ src/data/data.js
@@ -11,25 +11,29 @@ const initialData = {
       title: "Une caresse animée",
       content: {
         show: "Une caresse animée",
-        header:
-          "installation numérique et de performance -- digital performance installation",
         img: "./img/caresse_animee.jpg",
         alt:
           "A photograph of the artist in a minidress made of bubblewrap, back to the camera, facing a blue projection screen with the words 'no signal' written on it",
-        video: "https://player.vimeo.com/video/346746408",
-        videoTitle: "trailer",
-        introTextFr: "Lorem ipsum dolor",
-        textFr:
-          "sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis at consectetur lorem donec. Tempor orci eu lobortis elementum nibh tellus molestie nunc. Et leo duis ut diam quam nulla porttitor. Id cursus metus aliquam eleifend mi in. Interdum velit laoreet id donec ultrices tincidunt. Massa sed elementum tempus egestas sed sed risus pretium. Potenti nullam ac tortor vitae purus faucibus ornare suspendisse. Urna cursus eget nunc scelerisque viverra mauris in aliquam sem. Quisque id diam vel quam elementum. Augue interdum velit euismod in pellentesque massa placerat. Nulla facilisi nullam vehicula ipsum. Magna sit amet purus gravida quis blandit turpis. Consectetur adipiscing elit ut aliquam purus sit amet luctus venenatis. Vestibulum morbi blandit cursus risus at ultrices mi tempus imperdiet. Pellentesque elit eget gravida cum sociis.",
-        introTextEn: "Lorem ipsum dolor",
-        textEn:
-          "sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis at consectetur lorem donec. Tempor orci eu lobortis elementum nibh tellus molestie nunc. Et leo duis ut diam quam nulla porttitor. Id cursus metus aliquam eleifend mi in. Interdum velit laoreet id donec ultrices tincidunt. Massa sed elementum tempus egestas sed sed risus pretium. Potenti nullam ac tortor vitae purus faucibus ornare suspendisse. Urna cursus eget nunc scelerisque viverra mauris in aliquam sem. Quisque id diam vel quam elementum. Augue interdum velit euismod in pellentesque massa placerat. Nulla facilisi nullam vehicula ipsum. Magna sit amet purus gravida quis blandit turpis. Consectetur adipiscing elit ut aliquam purus sit amet luctus venenatis. Vestibulum morbi blandit cursus risus at ultrices mi tempus imperdiet. Pellentesque elit eget gravida cum sociis.",
+        header:
+          "installation numérique et de performance -- digital performance installation",
         status: "première 05/2021 : prête à tourner -- ready for touring",
+        video: "https://player.vimeo.com/video/459501770/f50b985e19",
+        videoTitle: "trailer",
+        introTextFr: "Des fenêtres vidéo s’ouvrent et se ferment dans",
+        textFr:
+          "une succession et une superposition d’espaces. Un “je”, s’adressant à sa webcam, s’imagine des mondes habitables pour fuir le chaos destructeur qui l’entoure. Comme un appel dans la nuit, je te dis que je t’aime pour établir ma présence. Pour nous réunir je suis devenu pixel. Depuis que le monde a basculé, j’arrive à rester au présent grâce à cette course à relais d’images et d’espaces. Comme pour poser un geste concret dans un monde de plus en plus informe.",
+        introTextEn: "Video windows open and close in",
+        textEn:
+          "an overlap of spaces. A character, addressing the webcam, imagines habitable worlds in order to escape the surrounding chaos. Like a calling at night, I tell you that I love you in order to feel my presence. To bring us together I have become pixels. Since this world has tipped over, I can stay in the present through this relay-course of images and spaces. As if to hook yourself to a moving mass. As if to do something real in a world that’s more and more shapeless.",
         credits: [
-          ["création -- created by", "Francois Bouvier"],
+          [
+            "création et interprétation -- creation and interpretation",
+            "Francois Bouvier",
+          ],
+          ["musique -- music", "Danilo de Campos Pacheco"],
           [
             "texte -- text",
-            "écrit par -- written by Tracy Wright, cité en -- quoted from 'Authenticity is a Feeling' (Jacob Wren)",
+            "écrit par -- written by Tracy Wright (tiré de l’essai 'Un sentiment d’authenticité' de Jacob Wren -- from 'Authenticity is a feeling' by Jacob Wren)",
           ],
         ],
       },
@@ -45,11 +49,22 @@ const initialData = {
         alt:
           "A figure, wearing black, photographed in the process of falling onto a blue mat",
         // video: "here's a vimeo link",
-        textFr: "Voilà le texte en français",
+        introTextFr: "Quel type de corps apparaît",
+        textFr:
+          "à la convergence des catastrophes climatiques et de la révolution digitale? Le corps qui ne fonctionne plus, immobilisé, fixé, paralysé; un corps flou, queer et sans cadre. Dans I miss Grandma so sad, c’est ce corps qui entre en scène. Naviguant des paysages virtuels et sonores changeants, ainsi qu’une scénographie de poésie et d’objets, il est engagé dans un processus de devenir qui le dépasse, sans cesse interrompu, en train de chuter, d’apparaître et de disparaître. De ce jeu résulte un spectacle intimiste et ironique à la fois, mêlant humour, cynisme et sincérité. Face à la « nature » changeante et un avenir incertain, pris entre les échos du numérique et les objets devenus obstacles, un corps découvre des repères mobiles pour recomposer un monde habitable.",
         textEn: "This is the content about une caresse animée",
         // credits: "Here are the credits",
         status:
           "rentre en période de production autonme 2021 -- production period autumn 2021",
+        credits: [
+          [
+            "création et interprétation -- creation and interpretation",
+            "Francois Bouvier",
+          ],
+          ["dramaturge -- dramaturg", "Sebastian Kann"],
+          ["oeil extérieur -- outside eye", "Peter James"],
+          ["voix additionnelle -- additional voice", "Cora Kim"],
+        ],
       },
     },
 
@@ -63,10 +78,22 @@ const initialData = {
         alt:
           "A figure, wearing black, photographed in the process of falling onto a blue mat",
         // video: "here's a vimeo link",
-        textFr: "Voilà le texte en français",
-        textEn: "This is the content about une caresse animée",
+        introTextFr: "Cirque les résidus est une",
+        textFr:
+          "collaboration entre François Bouvier, acrobate, et Sandrine Masse-Savard, musicienne.Depuis 2018, le duo crée et adapte des petites formes de spectacle en extérieur. Utilisant acrobatie, danse, fil de fer, poésie et musique live, le duo invente un univers ludique et invitant, afin de raconter l'histoire d'une rencontre et de proposer une attitude d'ouverture vers les autres. L'histoire d'une rencontre entre deux individus, qui apprennent à se connaître et à partager un espace, acceptant l'autre dans sa différence pour tisser des liens et bâtir une communauté.",
+        introTextEn: "Cirque les résidus is",
+        textEn:
+          "a 25 minutes circus performance that combines tight-wire, floor acrobatics, poetry, story-telling, quick-change, and live-music to present an uplifting proposal. The show follows the intertwining stories of two characters that meet and learn to share a common space, respecting each other in their differences, making kin and building a community.",
         // credits: "Here are the credits",
         status: "prête à tourner -- ready for touring",
+        credits: [
+          ["création -- creation", "Francois Bouvier + Sandrine Masse-Savard"],
+          ["musique -- music", "Sandrine Masse-Savard"],
+          [
+            "aide à la création -- creation help",
+            "Sebastian Kann + Anna-Karyna Barlati",
+          ],
+        ],
       },
     },
 
