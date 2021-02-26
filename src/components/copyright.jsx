b56edd90afdfb85208f47d01218d9049124f3352
Sebastian Kann <sebastiankann@gmail.com>
2021-02-26 10:55:19 -0500

    added icon credits to the footer

--- src/components/copyright.jsx
+++ src/components/copyright.jsx
@@ -4,7 +4,10 @@ function Copyright() {
   return (
     <div className="copyright">
       <p className="copyright--text">
-      codage -- code + design: <a className="copyright--link" href="https://github.com/seb-2002" rel="noreferrer" target="_blank">Sebastian Kann</a></p>
+      <span className="copyright--text-bold">codage -- code + design: </span><a className="copyright--link" href="https://github.com/seb-2002" rel="noreferrer" target="_blank">Sebastian Kann</a><span className="separator">&nbsp;||&nbsp;</span></p>
+     <p className="copyright--text copyright--text2">
+      <a className="copyright--link"  rel="noreferrer" target="_blank" href="https://icons8.com/icons/set/circus-tent"><span className="copyright--text-bold"> icône chapiteau -- tent icon:</span></a> <a className="copyright--link"  rel="noreferrer" target="_blank" href="https://icons8.com">Icons8</a>
+      </p>
     </div>
   )
 }
