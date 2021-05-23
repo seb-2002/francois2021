2d8b79515ce75c8973454d94530814262d76ea4a
Sebastian Kann <sebastiankann@gmail.com>
2021-05-23 11:36:12 -0400

    commented out data.js

--- src/data/data.js
+++ src/data/data.js
@@ -1,142 +1,142 @@
-const initialData = {
-  imgHome: {
-    src: "./img/footer_img.png",
-    alt: "the image on the home page",
-  },
-  boxes: [
-    {
-      title: "François Bouvier",
-      content: {
-        flipside: "Acrobate toi-même!",
-      },
-    },
+// const initialData = {
+//   imgHome: {
+//     src: "./img/footer_img.png",
+//     alt: "the image on the home page",
+//   },
+//   boxes: [
+//     {
+//       title: "François Bouvier",
+//       content: {
+//         flipside: "Acrobate toi-même!",
+//       },
+//     },
 
-    {
-      title: "Une caresse animée",
-      content: {
-        show: "Une caresse animée",
-        img: "./img/caresse_animee.jpg",
-        alt:
-          "A photograph of the artist in a minidress made of bubblewrap, back to the camera, facing a blue projection screen with the words 'no signal' written on it",
-        header:
-          "installation numérique et de performance -- digital performance installation",
-        statusEn: "premiere 05/2021 : ready for touring",
-        statusFr: "première 05/2021 : prête à tourner",
-        video: "https://player.vimeo.com/video/546926710",
-        videoTitle: "trailer",
-        introTextFr: "Des fenêtres vidéo s’ouvrent et se ferment dans",
-        // textFr:
-        //   "une succession et une superposition d’espaces. Un “je”, s’adressant à sa webcam, s’imagine des mondes habitables pour fuir le chaos destructeur qui l’entoure. Comme un appel dans la nuit, je te dis que je t’aime pour établir ma présence. Pour nous réunir je suis devenu pixel. Depuis que le monde a basculé, j’arrive à rester au présent grâce à cette course à relais d’images et d’espaces. Comme pour poser un geste concret dans un monde de plus en plus informe.",
-        introTextEn: "Video windows open and close in",
-        textEn:
-          "an overlap of spaces. A character, addressing the webcam, imagines habitable worlds in order to escape the surrounding chaos. Like a calling at night, I tell you that I love you in order to feel my presence. To bring us together I have become pixels. Since this world has tipped over, I can stay in the present through this relay-course of images and spaces. As if to hook yourself to a moving mass. As if to do something real in a world that’s more and more shapeless.",
-        credits: [
-          [
-            "création et interprétation -- creation and interpretation",
-            "Francois Bouvier",
-          ],
-          ["musique -- music", "Danilo de Campos Pacheco"],
-          [
-            "texte -- text",
-            "écrit par -- written by Tracy Wright (tiré de l’essai 'Un sentiment d’authenticité' de Jacob Wren -- from 'Authenticity is a feeling' by Jacob Wren)",
-          ],
-        ],
-      },
-    },
+//     {
+//       title: "Une caresse animée",
+//       content: {
+//         show: "Une caresse animée",
+//         img: "./img/caresse_animee.jpg",
+//         alt:
+//           "A photograph of the artist in a minidress made of bubblewrap, back to the camera, facing a blue projection screen with the words 'no signal' written on it",
+//         header:
+//           "installation numérique et de performance -- digital performance installation",
+//         statusEn: "premiere 05/2021 : ready for touring",
+//         statusFr: "première 05/2021 : prête à tourner",
+//         video: "https://player.vimeo.com/video/546926710",
+//         videoTitle: "trailer",
+//         introTextFr: "Des fenêtres vidéo s’ouvrent et se ferment dans",
+//         textFr:
+//           "une succession et une superposition d’espaces. Un “je”, s’adressant à sa webcam, s’imagine des mondes habitables pour fuir le chaos destructeur qui l’entoure. Comme un appel dans la nuit, je te dis que je t’aime pour établir ma présence. Pour nous réunir je suis devenu pixel. Depuis que le monde a basculé, j’arrive à rester au présent grâce à cette course à relais d’images et d’espaces. Comme pour poser un geste concret dans un monde de plus en plus informe.",
+//         introTextEn: "Video windows open and close in",
+//         textEn:
+//           "an overlap of spaces. A character, addressing the webcam, imagines habitable worlds in order to escape the surrounding chaos. Like a calling at night, I tell you that I love you in order to feel my presence. To bring us together I have become pixels. Since this world has tipped over, I can stay in the present through this relay-course of images and spaces. As if to hook yourself to a moving mass. As if to do something real in a world that’s more and more shapeless.",
+//         credits: [
+//           [
+//             "création et interprétation -- creation and interpretation",
+//             "Francois Bouvier",
+//           ],
+//           ["musique -- music", "Danilo de Campos Pacheco"],
+//           [
+//             "texte -- text",
+//             "écrit par -- written by Tracy Wright (tiré de l’essai 'Un sentiment d’authenticité' de Jacob Wren -- from 'Authenticity is a feeling' by Jacob Wren)",
+//           ],
+//         ],
+//       },
+//     },
 
-    {
-      title: "I miss grandma so sad",
-      content: {
-        show: "I miss grandma so sad",
-        header:
-          "solo de 45 min pour théatre -- 45-min solo performance for the black box",
-        img: "./img/i_miss_grandma.png",
-        alt:
-          "A figure, wearing black, photographed in the process of falling onto a blue mat",
-        video: "https://player.vimeo.com/video/546899256",
-        videoTitle: "trailer",
-        introTextFr: "Quel type de corps apparaît",
-        textFr:
-          "à la convergence des catastrophes climatiques et de la révolution digitale? Le corps qui ne fonctionne plus, immobilisé, fixé, paralysé; un corps flou, queer et sans cadre. Dans I miss Grandma so sad, c’est ce corps qui entre en scène. Naviguant des paysages virtuels et sonores changeants, ainsi qu’une scénographie de poésie et d’objets, il est engagé dans un processus de devenir qui le dépasse, sans cesse interrompu, en train de chuter, d’apparaître et de disparaître. De ce jeu résulte un spectacle intimiste et ironique à la fois, mêlant humour, cynisme et sincérité. Face à la « nature » changeante et un avenir incertain, pris entre les échos du numérique et les objets devenus obstacles, un corps découvre des repères mobiles pour recomposer un monde habitable.",
-        textEn: "This is the content about une caresse animée",
-        statusFr: "rentre en période de production autonme 2021",
-        statusEn: "production period autumn 2021",
-        credits: [
-          [
-            "création et interprétation -- creation and interpretation",
-            "Francois Bouvier",
-          ],
-          ["dramaturge -- dramaturg", "Sebastian Kann"],
-          ["oeil extérieur -- outside eye", "Peter James"],
-          ["voix additionnelle -- additional voice", "Cora Kim"],
-        ],
-      },
-    },
+//     {
+//       title: "I miss grandma so sad",
+//       content: {
+//         show: "I miss grandma so sad",
+//         header:
+//           "solo de 45 min pour théatre -- 45-min solo performance for the black box",
+//         img: "./img/i_miss_grandma.png",
+//         alt:
+//           "A figure, wearing black, photographed in the process of falling onto a blue mat",
+//         video: "https://player.vimeo.com/video/546899256",
+//         videoTitle: "trailer",
+//         introTextFr: "Quel type de corps apparaît",
+//         textFr:
+//           "à la convergence des catastrophes climatiques et de la révolution digitale? Le corps qui ne fonctionne plus, immobilisé, fixé, paralysé; un corps flou, queer et sans cadre. Dans I miss Grandma so sad, c’est ce corps qui entre en scène. Naviguant des paysages virtuels et sonores changeants, ainsi qu’une scénographie de poésie et d’objets, il est engagé dans un processus de devenir qui le dépasse, sans cesse interrompu, en train de chuter, d’apparaître et de disparaître. De ce jeu résulte un spectacle intimiste et ironique à la fois, mêlant humour, cynisme et sincérité. Face à la « nature » changeante et un avenir incertain, pris entre les échos du numérique et les objets devenus obstacles, un corps découvre des repères mobiles pour recomposer un monde habitable.",
+//         textEn: "This is the content about une caresse animée",
+//         statusFr: "rentre en période de production autonme 2021",
+//         statusEn: "production period autumn 2021",
+//         credits: [
+//           [
+//             "création et interprétation -- creation and interpretation",
+//             "Francois Bouvier",
+//           ],
+//           ["dramaturge -- dramaturg", "Sebastian Kann"],
+//           ["oeil extérieur -- outside eye", "Peter James"],
+//           ["voix additionnelle -- additional voice", "Cora Kim"],
+//         ],
+//       },
+//     },
 
-    {
-      title: "Cirque les Residus",
-      content: {
-        show: "Cirque les Residus",
-        header:
-          "spectacle queer familial pour extérieur -- queer family show for outside",
-        img: "./img/les_residus.jpg",
-        alt:
-          "A figure, wearing black, photographed in the process of falling onto a blue mat",
-        video: "https://player.vimeo.com/video/363546117",
-        videoTitle: "trailer",
-        introTextFr: "Cirque les résidus est une",
-        textFr:
-          "collaboration entre François Bouvier, acrobate, et Sandrine Masse-Savard, musicienne. Depuis 2018, le duo crée et adapte des petites formes de spectacle en extérieur. Utilisant acrobatie, danse, fil de fer, poésie et musique live, le duo invente un univers ludique et invitant, afin de raconter l'histoire d'une rencontre et de proposer une attitude d'ouverture vers les autres. L'histoire d'une rencontre entre deux individus, qui apprennent à se connaître et à partager un espace, acceptant l'autre dans sa différence pour tisser des liens et bâtir une communauté.",
-        introTextEn: "Cirque les résidus is",
-        textEn:
-          "a 25 minutes circus performance that combines tight-wire, floor acrobatics, poetry, story-telling, quick-change, and live-music to present an uplifting proposal. The show follows the intertwining stories of two characters that meet and learn to share a common space, respecting each other in their differences, making kin and building a community.",
-        statusFr: "prête à tourner",
-        statusEn: "ready for touring",
-        credits: [
-          ["création -- creation", "Francois Bouvier + Sandrine Masse-Savard"],
-          ["musique -- music", "Sandrine Masse-Savard"],
-          [
-            "aide à la création -- creation help",
-            "Sebastian Kann + Anna-Karyna Barlati",
-          ],
-        ],
-      },
-    },
+//     {
+//       title: "Cirque les Residus",
+//       content: {
+//         show: "Cirque les Residus",
+//         header:
+//           "spectacle queer familial pour extérieur -- queer family show for outside",
+//         img: "./img/les_residus.jpg",
+//         alt:
+//           "A figure, wearing black, photographed in the process of falling onto a blue mat",
+//         video: "https://player.vimeo.com/video/363546117",
+//         videoTitle: "trailer",
+//         introTextFr: "Cirque les résidus est une",
+//         textFr:
+//           "collaboration entre François Bouvier, acrobate, et Sandrine Masse-Savard, musicienne. Depuis 2018, le duo crée et adapte des petites formes de spectacle en extérieur. Utilisant acrobatie, danse, fil de fer, poésie et musique live, le duo invente un univers ludique et invitant, afin de raconter l'histoire d'une rencontre et de proposer une attitude d'ouverture vers les autres. L'histoire d'une rencontre entre deux individus, qui apprennent à se connaître et à partager un espace, acceptant l'autre dans sa différence pour tisser des liens et bâtir une communauté.",
+//         introTextEn: "Cirque les résidus is",
+//         textEn:
+//           "a 25 minutes circus performance that combines tight-wire, floor acrobatics, poetry, story-telling, quick-change, and live-music to present an uplifting proposal. The show follows the intertwining stories of two characters that meet and learn to share a common space, respecting each other in their differences, making kin and building a community.",
+//         statusFr: "prête à tourner",
+//         statusEn: "ready for touring",
+//         credits: [
+//           ["création -- creation", "Francois Bouvier + Sandrine Masse-Savard"],
+//           ["musique -- music", "Sandrine Masse-Savard"],
+//           [
+//             "aide à la création -- creation help",
+//             "Sebastian Kann + Anna-Karyna Barlati",
+//           ],
+//         ],
+//       },
+//     },
 
-    {
-      title: "media",
-      content: {
-        flipside: "à venir -- coming soon",
-      },
-    },
+//     {
+//       title: "media",
+//       content: {
+//         flipside: "à venir -- coming soon",
+//       },
+//     },
 
-    {
-      title: "agenda",
-      content: {
-        flipside: "à venir -- coming soon",
-      },
-    },
+//     {
+//       title: "agenda",
+//       content: {
+//         flipside: "à venir -- coming soon",
+//       },
+//     },
 
-    {
-      title: "bio",
-      content: {
-        title: "bio",
-        bioFr:
-          "Originaire de Gatineau, François a une pratique acrobatique depuis l’âge de cinq ans. Dans ses recherches actuelles, il s’intéresse à la cohabitation de plusieurs éléments sur scène (corps acrobatique, images digitales  et paysages, poésie, récits collectifs et histoires personnelles). Par un travail d’improvisation, François collectionne les rencontres entre ces éléments sur scène, tissant un réseau sensible de liens et de répercussions, transformant les objets en images et le corps en histoire.",
-        bioEn:
-          "Originally from Gatineau, François has practiced acrobatics since the age of 5. In his most recent work, he is interested in what emerges when different elements—the acrobatic body, landscapes and digital imagery, personal and collective stories, poetry—are invited to cohabit a space. Working through improvisation, François collects encounters between these elements, weaving a network of relations and repercussions, transforming objects into images and the body into a story.",
-      },
-    },
-    {
-      title: "contact",
-      content: {
-        contact: {
-          name: "bouvi.francois",
-          domain: "gmail.com",
-        },
-      },
-    },
-  ],
-};
+//     {
+//       title: "bio",
+//       content: {
+//         title: "bio",
+//         bioFr:
+//           "Originaire de Gatineau, François a une pratique acrobatique depuis l’âge de cinq ans. Dans ses recherches actuelles, il s’intéresse à la cohabitation de plusieurs éléments sur scène (corps acrobatique, images digitales  et paysages, poésie, récits collectifs et histoires personnelles). Par un travail d’improvisation, François collectionne les rencontres entre ces éléments sur scène, tissant un réseau sensible de liens et de répercussions, transformant les objets en images et le corps en histoire.",
+//         bioEn:
+//           "Originally from Gatineau, François has practiced acrobatics since the age of 5. In his most recent work, he is interested in what emerges when different elements—the acrobatic body, landscapes and digital imagery, personal and collective stories, poetry—are invited to cohabit a space. Working through improvisation, François collects encounters between these elements, weaving a network of relations and repercussions, transforming objects into images and the body into a story.",
+//       },
+//     },
+//     {
+//       title: "contact",
+//       content: {
+//         contact: {
+//           name: "bouvi.francois",
+//           domain: "gmail.com",
+//         },
+//       },
+//     },
+//   ],
+// };
 
-export default initialData;
+// export default initialData;
